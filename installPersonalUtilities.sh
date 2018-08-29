c5dc6820a7b5b02ab7d453d926171da9a0d35a70
Muhammet Küçük <iletisim@muhammetkucuk.com>
2018-08-29 10:20:33 +0300

    script update

--- installPersonalUtilities.sh
+++ installPersonalUtilities.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 sudo apt-get update -y &&
 sudo apt-get upgrade -y &&
+sudo apt-get dist-upgrade -y &&
+sudo apt-get autoremove -y &&
+sudo apt-get install build-essential cmake cmake-qt-gui -y &&
 sudo apt-get install git -y
 sudo apt-get install libcanberra-gtk-module:i386 -y
 sudo apt install libcanberra-gtk-module libcanberra-gtk3-module -y &&
@@ -14,7 +17,7 @@ sudo cp -r node-v8.11.4-linux-x64/{bin,include,lib,share}  /usr/ &&
 sudo npm i -g pm2 nodemon n
 rm node-v8.11.4-linux-x64.tar.xz &&
 rm -R node-v8.11.4-linux-x64 &&
-wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb && 
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb &&
 sudo dpkg -i teamviewer_amd64.deb &&
 rm teamviewer_amd64.deb &&
 wget https://repo.skype.com/latest/skypeforlinux-64.deb &&
@@ -29,5 +32,6 @@ rm atom-amd64.deb &&
 sudo snap install spotify &&
 sudo snap install vlc &&
 wget http://download.qt.io/archive/qt/5.11/5.11.1/qt-opensource-linux-x64-5.11.1.run &&
-chmod +x qt-opensource-linux-x64-5.11.1.run
-
+chmod +x qt-opensource-linux-x64-5.11.1.run &&
+sudo ldconfig -v &&
+sudo ldconfig
